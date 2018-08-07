e47604dc1f097351b1aae03c74f51112afa1abbd
Roland Tecson <roland_tecson@hotmail.com>
2018-08-07 12:25:01 -0700

    Code from lecture

--- w2d2-demo/ViewController.m
+++ w2d2-demo/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Create a frame for my view, x,y is 10,10 and width/height is 100x100
+    CGRect myFrame = CGRectMake(10.0, 10.0, 100.0, 100.0);
+    
+    // Create a view using myFrame as position and size
+    UIView *myView = [[UIView alloc] initWithFrame:myFrame];
+    
+    UIColor *myColor = [UIColor purpleColor];
+    [myView setBackgroundColor:myColor];
+    
+    // Add myView to my root view
+    [self.view addSubview:myView];
+    
+    // Change the value of myLabel (which is an outlet to a label in the storyboard
+    self.myLabel.text = @"Hi Bob";
+    
+    // Start value of animation
+    myView.alpha = 1.0;
+//    [UIView animateWithDuration:3.0 animations:^{
+//        // End value or result of animation
+////        myView.alpha = 0.0;
+//        myView.frame = CGRectMake(20.0, 30.0, 20.0, 150.0);
+//        myView.backgroundColor = [UIColor orangeColor];
+//    }];
+    
+    // Another variant of animateWithDuration, this one takes a completion block, which is executed at the end of the animation
+    [UIView animateWithDuration:3.0 animations:^{
+        myView.frame = CGRectMake(20.0, 30.0, 20.0, 150.0);
+        myView.backgroundColor = [UIColor orangeColor];
+    } completion:^(BOOL finished) {
+        // At this point, animation is completed
+        self.myLabel.text = @"Finished animation";
+    }];
 }
 
 
